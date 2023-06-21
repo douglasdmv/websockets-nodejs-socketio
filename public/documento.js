b90d8f33b25ea3c5e1f985f877a83833c0534028
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2023-06-20 23:00:43 -0300

    configurando socket.io

--- public/documento.js
+++ public/documento.js
@@ -1,7 +1,24 @@
-const socket = io()
+import { emitirTextoEditor, selecionarDocumento } from "./socket-front-documento.js"
+
+const parametros = new URLSearchParams(window.location.search)
+const nomeDocumento = parametros.get("nome")
 
 const textoEditor = document.getElementById("editor-texto")
+const tituloDocumento = document.getElementById("titulo-documento")
+
+tituloDocumento.textContent = nomeDocumento || "Documento sem título"
+
+selecionarDocumento(nomeDocumento)
 
 textoEditor.addEventListener("keyup", () => {
-    socket.emit("texto_editor", textoEditor.value)
+    emitirTextoEditor({
+        texto: textoEditor.value, 
+        nomeDocumento
+    })
 })
+
+function atualizarTextoEditor(texto) {
+    textoEditor.value = texto
+}
+
+export { atualizarTextoEditor }
--- src/socket-back.js
+++ src/socket-back.js
@@ -1,8 +1,44 @@
+import e from "express"
 import io from "./servidor.js"
+
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de javascript...",
+    },
+    {
+        nome: "Node",
+        texto: "texto de node...",
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de socket.io...",
+    }
+]
 
 io.on("connection", (socket) => {
     console.log(`Usuário conectado: ${socket.id}`)
-    socket.on("texto_editor", (texto) => {
-        console.log(texto)
+
+    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
+        socket.join(nomeDocumento)
+        const documento = encontrarDocumento(nomeDocumento)
+        if(documento) {
+            devolverTexto(documento.texto)
+        }
+    })
+
+    socket.on("texto_editor", ({ texto, nomeDocumento }) => {
+        const documento = encontrarDocumento(nomeDocumento)
+        if(documento) {
+            documento.texto = texto
+            socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+        }
     })
 })
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome
+    })
+    return documento
+}
