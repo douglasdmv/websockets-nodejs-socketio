63aad22eed98df21ba9d879f7c2e24eb7c10352c
Douglas Viegas <douglasmviegas@gmail.com>
2023-06-22 16:49:12 -0300

    criando cadastro de usuarios

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -2,15 +2,16 @@ import { MongoClient } from "mongodb"
 
 const cliente = new MongoClient("mongodb://alura:123@ac-zuijanp-shard-00-00.ocgqvoa.mongodb.net:27017,ac-zuijanp-shard-00-01.ocgqvoa.mongodb.net:27017,ac-zuijanp-shard-00-02.ocgqvoa.mongodb.net:27017/?ssl=true&replicaSet=atlas-5amxkb-shard-0&authSource=admin&retryWrites=true&w=majority")
 
-let documentosColecao
+let documentosColecao, usuariosColecao
 
 try {
     await cliente.connect()
     const db = cliente.db("alura-websockets")
     documentosColecao = db.collection("documentos")
+    usuariosColecao = db.collection("usuarios")
     console.log("Conectado ao banco de dados com sucesso!")
 } catch (erro) {
     console.log(erro)
 }
 
-export { documentosColecao }
+export { documentosColecao, usuariosColecao }
