67abc9ad36286131b0717ae10434cb8aea5d559b
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2023-06-24 15:04:41 -0300

    entrada de saída de usuários nos documentos

--- server/registrarEventos/documento.js
+++ server/registrarEventos/documento.js
@@ -1,5 +1,5 @@
 import { atualizaDocumento, encontrarDocumento, excluirDocumento } from "../db/documentosDb.js"
-import { adicionarConexao, obterUsuariosDocumento } from "../utils/conexoesDocumentos.js"
+import { adicionarConexao, obterUsuariosDocumento, removerConexao } from "../utils/conexoesDocumentos.js"
 
 function registrarEventosDocumento(socket, io) {
     socket.on("selecionar_documento", async ({ nomeDocumento, nomeUsuario }, devolverTexto) => {
@@ -12,20 +12,26 @@ function registrarEventosDocumento(socket, io) {
             io.to(nomeDocumento).emit("usuarios_no_documento", usuariosNoDocumento)
             devolverTexto(documento.texto)
         }
-    })
 
-    socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
-        const atualizacao = await atualizaDocumento(nomeDocumento, texto)
-        if(atualizacao.modifiedCount) {
-            socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
-        }
-    })
+        socket.on("texto_editor", async ({ texto, nomeDocumento }) => {
+            const atualizacao = await atualizaDocumento(nomeDocumento, texto)
+            if(atualizacao.modifiedCount) {
+                socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+            }
+        })
+    
+        socket.on("excluir_documento", async (nome) => {
+            const resultado = await excluirDocumento(nome)
+            if(resultado.deletedCount) {
+                io.emit("excluir_documento_sucesso", nome)
+            }
+        })
 
-    socket.on("excluir_documento", async (nome) => {
-        const resultado = await excluirDocumento(nome)
-        if(resultado.deletedCount) {
-            io.emit("excluir_documento_sucesso", nome)
-        }
+        socket.on("disconnect", () => {
+            removerConexao(nomeDocumento, nomeUsuario)
+            const usuariosNoDocumento = obterUsuariosDocumento(nomeDocumento)
+            io.to(nomeDocumento).emit("usuarios_no_documento", usuariosNoDocumento)
+        })
     })
 }
 
